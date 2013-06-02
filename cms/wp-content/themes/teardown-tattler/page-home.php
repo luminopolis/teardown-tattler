d38d7e93c442b55c9bc4c952fe1839c3a07e549d
Briston Davidge <bristond@gmail.com>
2013-06-02 14:30:16 -0500

    homepage edits

--- cms/wp-content/themes/teardown-tattler/page-home.php
+++ cms/wp-content/themes/teardown-tattler/page-home.php
@@ -13,8 +13,12 @@ get_header(); ?>
 <div class="jumbotron masthead">
     <div class="container">
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	
+
       <h1><?php the_title();?></h1>
       <?php the_content();?>
+	
+
     </div>
 </div>
 
@@ -22,20 +26,25 @@ get_header(); ?>
 <div class="container">
   <div class="marketing">
   <div class="row-fluid">
-    <div class="span4">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-left" );
-?>
+    <div class="span5">
+      <?php if( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-left" ); ?>
+	<a class="btn btn-primary btn-large" href="<?php echo site_url(); ?>/signup">Take Action</a>
+    </div>
+    <div class="span2">
+    <!--  <?php if( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-middle" ); ?>-->
     </div>
     <div class="span4">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-middle" );
-?>
-    </div>
-    <div class="span4">
-      <?php
-if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-right" );
-?>
+		<h2>Endangered Buildings</h2>
+		<ul class="nav nav-pills nav-stacked">
+			<?php
+				//if ( function_exists( 'dynamic_sidebar' ) ) dynamic_sidebar( "home-right" );
+				$results = teardown_get_properties( 5 );
+				
+				foreach( $results as $result ) :
+					echo "<li><a href='#'>" . $result->address_line_1 . "</a></li>";
+				endforeach;
+			?>
+		</ul>
     </div>
   </div>
 </div><!-- /.marketing -->
