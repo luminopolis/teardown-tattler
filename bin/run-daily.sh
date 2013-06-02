c064a9ec8b61428653fad29ed66882a1ebc7785a
Paul Barham <paulb@svagesoft.com>
2013-06-02 15:37:28 -0500

    Initial daily run code - needs testing and needs curl help

---
+++ bin/run-daily.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+FILENAME="daily.xls"
+#
+# Find out where we are running from
+#  Go there
+#  Hop over to the WP directory
+#
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+cd ../cms
+
+WPDIR=`pwd`
+
+cd ../data
+
+DATADIR=`pwd`
+
+curl -k -o $FILENAME  https://data.kcmo.org/311/311-Data-for-At-Risk-Buildings/p2je-p96s
+
+cd $WPDIR
+
+php extract-311.php \
+	-f $FILENAME \
+	-d $DATADIR
+
+
+php notify-maillist.php
+
